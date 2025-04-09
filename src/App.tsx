6d901567511e14d84e4893fe5b054860421dfe77
uplanojevicpax8 <uplanojevic@pax8.com>
2025-04-09 10:33:51 +0200

    #2: add private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboards from './components/views/Dashboards';
+import Transactions from './components/views/Transactions';
 import Login from './components/views/Login';
-import { useUser } from './context/UserContext';
+import PrivateRoute from './routes/PrivateRoute';
 
 // BrowserRouter - top-level wrapper that enables routing
 // Routes - container that holds all your individual route definitions
 // Route - defines a URL path and which component to render at that path
-function App() {
-  const { user } = useUser();
-  
+function App() {  
   return (
     <BrowserRouter>
-      {user?.isAuthenticated && <Dashboards />}
       <Routes>
         <Route path="/" element={<Login />} />
+
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Dashboards />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/transactions"
+          element={
+            <PrivateRoute>
+              <Transactions />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
