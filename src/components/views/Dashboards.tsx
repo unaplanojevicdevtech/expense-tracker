1061f396498950652802b2fa21a9f0ce2cbe5411
unaplanojevicdevtech <una.planojevic@devtechgroup.com>
2025-08-07 18:50:34 +0200

    #4: Dashboard page - pie chart

--- src/components/views/Dashboards.tsx
+++ src/components/views/Dashboards.tsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
 import Header from "../Header";
+import { PieChart } from '@mui/x-charts/PieChart';
+import transactions from '../../data/transactions.json';
 
 function Dashboards() {
+  const [ transactionList ] = useState(transactions);
+
+  const categoryCurrencyTotals: { [key: string]: { [currency: string]: number } } = {};
+  for (const transaction of transactionList) {
+    if (!categoryCurrencyTotals[transaction.category]) {
+      categoryCurrencyTotals[transaction.category] = {};
+    }
+    categoryCurrencyTotals[transaction.category][transaction.currency] =
+      (categoryCurrencyTotals[transaction.category][transaction.currency] || 0) + transaction.amount;
+  }
+
+  const pieData = [];
+  let id = 0;
+  for (const [category, currencyObj] of Object.entries(categoryCurrencyTotals)) {
+    for (const [currency, value] of Object.entries(currencyObj)) {
+      pieData.push({
+        id: id++,
+        value,
+        label: `${category} (${currency})`,
+      });
+    }
+  }
+
   return (
     <>
       <Header />
       <div>
-        <h1>Dashboards</h1>
-        <p>Welcome to the Dashboards page!</p>
+        <main>
+          <h1>Dashboards</h1>
+          <div>
+            <PieChart
+              series={[
+                {
+                  data: [...pieData],
+                  innerRadius: 20,
+                  outerRadius: 100,
+                  paddingAngle: 5,
+
+                }
+              ]}
+              width={200}
+              height={200}
+            />
+          </div>
+        </main>
       </div>
     </>
   );
